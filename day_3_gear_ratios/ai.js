f5efda4b2fe510b73cd25f924ed2f63ec49a6b4a
Exiled Spirit <gab.gabriel.1002@outlook.com>
2024-08-07 01:10:15 -0300

    day 3: no progress :(

--- day_3_gear_ratios/ai.js
+++ day_3_gear_ratios/ai.js
@@ -1,49 +1,33 @@
-// // Reading the data
-// const fs = require('node:fs');
-
-// let data;
-
-// try {
-//   data = fs.readFileSync('./input_2.txt', 'utf8');
-// } catch (err) {
-//   console.error(err);
-//   throw err;
-// }
-
 const data = process.argv[2];
 
-if (typeof data !== `string`) throw new Error('Erro ao ler os dados');
+if (typeof data !== `string`) throw new Error('Error reading the data');
 
 const lineList = data.split('\n');
-lineList.map((line) => {
-  return line.substring(0, 140);
-});
-
 const MAX_X = lineList.length;
 const MAX_Y = lineList[0].length;
-console.log(MAX_X)
-console.log(MAX_Y)
+
+console.log(MAX_X);
+console.log(MAX_Y);
 
 const grid = new Array(MAX_X).fill(null).map(() => new Array(MAX_Y).fill(null));
 
 let incompleteNumber = '';
 
-for(let i = 0; i < MAX_X; i++) {
-  for(let k = 0; k < MAX_Y; k++) {
+for (let i = 0; i < MAX_X; i++) {
+  for (let k = 0; k < MAX_Y; k++) {
     const char = lineList[i].charAt(k);
-    
     if (isNaN(char)) {
       insertNumberIntoArrayIfHasValue(i, k);
       addItemIntoArray(i, k, `${i}-${k}`, 'symbol', char, false);
     } else {
       incompleteNumber += char;
-      incompleteNumber.trim();
     }
   }
+  insertNumberIntoArrayIfHasValue(i, MAX_Y); // Handle incomplete number at the end of the line
 }
 
-for(let i = 0; i < MAX_X; i++) {
-  for(let k = 0; k < MAX_Y; k++) {
+for (let i = 0; i < MAX_X; i++) {
+  for (let k = 0; k < MAX_Y; k++) {
     if (grid[i][k]?.type === 'symbol' && grid[i][k]?.value !== '.') {
       turnAdjacentsOn(i, k);
     }
@@ -55,35 +39,30 @@ let sum = 0;
 
 for (let i = 0; i < MAX_X; i++) {
   for (let k = 0; k < MAX_Y; k++) {
-      if (grid[i][k] && !usedIds.includes(grid[i][k]?.id)) {
-        const adjacent = grid[i].some((item) => item?.id === grid[i][k]?.id && item?.adjacent);
+    if (grid[i][k] && !usedIds.includes(grid[i][k]?.id)) {
+      const adjacent = grid[i].some((item) => item?.id === grid[i][k]?.id && item?.adjacent);
+      usedIds.push(grid[i][k].id);
 
-        // const isSymbol = grid[i][k].type === 'symbol' && grid[i][k].value !== '.';
-        // let color = isSymbol ? '\x1b[32m' : (adjacent ? '\x1b[31m' : '\x1b[30m')
-        // process.stdout.write(`${color}${grid[i][k].value.toString()}`);
-        usedIds.push(grid[i][k].id)
-
-        if (adjacent) {
-          sum += grid[i][k].value
-        }
+      if (adjacent) {
+        sum += grid[i][k].value;
       }
+    }
   }
-  process.stdout.write('\n');
 }
 
 console.log(sum);
 
 function insertNumberIntoArrayIfHasValue(i, k) {
-  if (incompleteNumber != '') {
+  if (incompleteNumber !== '') {
     let iValue = i;
     let kValue = k;
-    if (k === 0 ) {
-        iValue = i - 1;
-        kValue = MAX_Y;
+    if (k === 0) {
+      iValue = i - 1;
+      kValue = MAX_Y;
     }
 
     for (let j = 0; j < incompleteNumber.length; j++) {
-      const id = `${iValue}-${kValue-1}`;
+      const id = `${iValue}-${kValue - 1}`;
       addItemIntoArray(iValue, kValue - (j + 1), id, 'number', parseInt(incompleteNumber), false);
     }
 
@@ -92,7 +71,7 @@ function insertNumberIntoArrayIfHasValue(i, k) {
 }
 
 function addItemIntoArray(i, k, id, type, value, adjacent = false, invalid = false) {
-  grid[i][k] = {id, type, value, adjacent, invalid};
+  grid[i][k] = { id, type, value, adjacent, invalid };
 }
 
 function turnAdjacentsOn(i, k) {
@@ -112,7 +91,6 @@ function turnAdjacentsOn(i, k) {
   if (i > 0 && grid[i - 1][k]?.type === 'number') {
     grid[i - 1][k].adjacent = true;
   }
-  
   // Check down-right diagonal
   if (i < MAX_X - 1 && k < MAX_Y - 1 && grid[i + 1][k + 1]?.type === 'number') {
     grid[i + 1][k + 1].adjacent = true;
