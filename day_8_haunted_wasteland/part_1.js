0971e793074a6163ab98dc108ede6d145c2090b5
Gabriel Gonçalvas <gabriel.goncalves@drz.local>
2024-08-12 13:34:21 -0300

    day 8: part one ez +- 30 min

--- day_8_haunted_wasteland/part_1.js
+++ day_8_haunted_wasteland/part_1.js
@@ -4,10 +4,40 @@ const fs = require('node:fs');
 let data;
 
 try {
-  data = fs.readFileSync('./sample.txt', 'utf8');
+  data = fs.readFileSync('./input.txt', 'utf8');
 } catch (err) {
   console.error(err);
   throw err;
 }
 
-console.log(data);
+const directions = data.split('\r\n\r\n')[0].split('');
+const instructions = data.split('\r\n\r\n')[1].split('\r\n');
+
+console.log(directions)
+console.log(instructions)
+
+const instructionsMap = new Map();
+
+instructions.forEach((instruction) => {
+  const chave = instruction.split(' = ')[0];
+  const leftRightKeys = instruction.split(' = ')[1].replace('(', '').replace(')', '').split(', ')
+  instructionsMap.set(chave, {L: leftRightKeys[0], R: leftRightKeys[1]});
+})
+
+console.log(instructionsMap)
+console.log(instructionsMap.get('AAA'))
+fromAtoZ(directions, instructionsMap)
+
+function fromAtoZ(directions, instructionMap) {
+  let currentPoint = 'AAA';
+  let count = 0;
+  
+  while(currentPoint !== 'ZZZ') {
+    const directionIndex = count % directions.length;
+    const currentPointObject = instructionMap.get(currentPoint);
+    currentPoint = currentPointObject[directions[directionIndex]];
+    count++;
+  }
+
+  console.log(count)
+}
