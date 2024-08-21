fe0c157d99c20429b56b46f687b81f7b778a128e
Exiled Spirit <gab.gabriel.1002@outlook.com>
2024-08-21 20:29:56 -0300

    day 11: criando README.md e formatacoes iniciais do arquivo

--- day_11_cosmic_expansion/part_1.js
+++ day_11_cosmic_expansion/part_1.js
@@ -10,3 +10,36 @@ try {
 }
 
 process.stdout.write(data);
+
+// processing input into grid
+const grid = data
+            .split('\n')
+            .map(line => line.trim())
+            .map(line => line.split(''));
+
+const GRID_X = grid.length;
+const GRID_Y = grid[0].length;
+
+let expandedGrid = Array.from({ length: GRID_X }, () => Array(GRID_Y).fill(null));
+console.log(grid)
+let rowsToExpand = [];
+let colsToExpand = [];
+
+for (let x = 0; x < GRID_X; x++) {
+    let hasGalaxyRow = false;
+    let hasGalaxyCol = false;
+
+    for (let y = 0; y < GRID_Y; y++) {
+        if (grid[x][y] == '#') hasGalaxyRow = true;
+        if (grid[y][x] == '#') hasGalaxyCol = true;
+    }
+    if (!hasGalaxyRow) rowsToExpand.push(x);
+    if (!hasGalaxyCol) colsToExpand.push(x);
+}
+
+console.log(colsToExpand);
+console.log(rowsToExpand);
+
+// Pair combinations = n * (n - 1) / 2
+
+
