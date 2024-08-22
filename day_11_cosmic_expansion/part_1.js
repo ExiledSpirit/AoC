fe5fa9f5f2b2cf95e4e89226b93e1b2272907b0c
Gabriel Gonçalvas <gabriel.goncalves@drz.local>
2024-08-22 13:55:02 -0300

    day 11: part 1 tranquilo

--- day_11_cosmic_expansion/part_1.js
+++ day_11_cosmic_expansion/part_1.js
@@ -3,7 +3,7 @@ const fs = require('node:fs');
 let data;
 
 try {
-    data = fs.readFileSync('./sample.txt', 'utf-8');
+    data = fs.readFileSync('./input.txt', 'utf-8');
 } catch (err) {
     console.error(err);
     throw err;
@@ -20,8 +20,6 @@ const grid = data
 const GRID_X = grid.length;
 const GRID_Y = grid[0].length;
 
-let expandedGrid = Array.from({ length: GRID_X }, () => Array(GRID_Y).fill(null));
-console.log(grid)
 let rowsToExpand = [];
 let colsToExpand = [];
 
@@ -37,9 +35,56 @@ for (let x = 0; x < GRID_X; x++) {
     if (!hasGalaxyCol) colsToExpand.push(x);
 }
 
-console.log(colsToExpand);
-console.log(rowsToExpand);
+const MAX_X = GRID_X + rowsToExpand.length;
+const MAX_Y = GRID_Y + colsToExpand.length;
+
+const galaxyList = [];
+const expandedGrid = Array.from({ length: MAX_X }, () => Array(MAX_Y).fill('.'));
+
+let rowCount = 0;
+
+for (let x = 0; x < GRID_X; x++) {
+    if (rowsToExpand.includes(x)) rowCount++;
+    
+    let colCount = 0;
+    for (let y = 0; y < GRID_Y; y++) {
+        if (colsToExpand.includes(y)) colCount++;
+        expandedGrid[x+rowCount][y+colCount] = grid[x][y];
+        if (grid[x][y] == '#') galaxyList.push([x+rowCount, y+colCount]);
+    }
+}
+
+function galaxyDistanceSum(galaxySet) {
+    let totalSum = 0;
+    let totalPairs = 0;
+    for (let x = 0; x < galaxySet.length; x++) {
+        const xGalaxy = galaxySet[x];
+        for (let y = x + 1; y < galaxySet.length; y++) {
+            totalPairs++;
+            const yGalaxy = galaxySet[y];
+            const xDistance = yGalaxy[0] - xGalaxy[0];
+            const yDistance = yGalaxy[1] > xGalaxy[1] ? yGalaxy[1] - xGalaxy[1] : xGalaxy[1] - yGalaxy[1];
+            const xySum = xDistance + yDistance;
+            totalSum += xySum;
+        }
+    }
+    console.log(totalPairs)
+    return totalSum;
+}
+
+console.log(galaxyDistanceSum(galaxyList));
+
+function printGrid(gridArray) {
+    const MAX_I = gridArray.length;
+    const MAX_J = gridArray[0].length;
+
+    for (let x = 0; x < MAX_I; x++) {
+        process.stdout.write(`${x}: `);
+        for (let y = 0; y < MAX_J; y++) {
+            process.stdout.write(gridArray[x][y]);
+        }
+        process.stdout.write('\n');
+    }
+}
 
 // Pair combinations = n * (n - 1) / 2
-
-
